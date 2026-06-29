1c3336029ed787b69bd8faffc0995534bc58bed6
Burhan Sancakli <burhan.sancakli@gmail.com>
2026-06-29 14:24:49 +0200

    deleted unused script

--- sanity.py
+++
@@ -1,8 +0,0 @@
-from observations import gamestate_to_observation, OBS_SIZE, pretty_print_observation
-import numpy as np
-
-# Test with empty state
-obs = gamestate_to_observation({})
-print(f"OBS_SIZE = {OBS_SIZE}")
-print(f"obs.shape = {obs.shape}")
-pretty_print_observation(obs)
